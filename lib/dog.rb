326ffad698d76454475f4f299f79dd50aebe0d5c
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-06 17:17:47 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+  def initialize(name)
+    @name = name
+  end 
+  
+  def name
+    @name
+  end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+rusty = Dog.new("Rusty") 
+rusty.name
+
+huskie = Dog.new("Huskie")
+huskie.breed
